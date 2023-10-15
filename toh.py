ca7a3bf6b85568da2fe8a349d375d7b93dd1cf3a
K. V. M. S. LOKESH <142161415+K-V-M-S@users.noreply.github.com>
2023-10-15 09:46:34 +0530

    Update toh.py

--- toh.py
+++ toh.py
@@ -1,47 +1,41 @@
-def TowerOfHanoi(n , source, destination, auxiliary):
+def toh(n , src, dest, aux, step=1):
     if n==1:
-        print ("Move disk 1 from peg",source,"to peg",destination)
-        if source == 'A':
+        print ('Step ',step, ": Move disk 1 from peg",src,"to peg",dest)
+        if src == 'A':
             A.remove(1)
-        elif source == 'B':
+        elif src == 'B':
             B.remove(1)
         else:
             C.remove(1)
-        if destination == 'A':
+        if dest == 'A':
             A.append(1)
-        elif destination == 'B':
+        elif dest == 'B':
             B.append(1)
         else:
             C.append(1)
-        print("A:", *A)
-        print("B:", *B)
-        print("C:", *C)
-        return
-    TowerOfHanoi(n-1, source, auxiliary, destination)
-    print ("Move disk",n,"from peg",source,"to peg",destination)
-    if source == 'A':
+        print("A:", *A,'\nB:',*B,'\nC:',*C)
+        return step+1
+    step = toh(n-1, src, aux, dest, step)
+    print ('Step ',step, ": Move disk",n,"from peg",src,"to peg",dest)
+    if src == 'A':
         A.remove(n)
-    elif source == 'B':
+    elif src == 'B':
         B.remove(n)
     else:
         C.remove(n)
-    if destination == 'A':
+    if dest == 'A':
         A.append(n)
-    elif destination == 'B':
+    elif dest == 'B':
         B.append(n)
     else:
         C.append(n)
-    print("A:", *A)
-    print("B:", *B)
-    print("C:", *C)
-    TowerOfHanoi(n-1, auxiliary, destination, source)
+    print("A:", *A,'\nB:',*B,'\nC:',*C)
+    step = toh(n-1, aux, dest, src, step+1)
+    return step
 
 n = int(input('Enter number of disks : '))
-A = [i for i in range(n, 0, -1)]
-B = []
-C = []
-print("A:", *A)
-print("B:", *B)
-print("C:", *C)
-TowerOfHanoi(n,'A','C','B')
-print('Goal Reached !')
+A,B,C = [i for i in range(n, 0, -1)],[],[]
+print('Initial State : ')
+print("A:", *A,'\nB:',*B,'\nC:',*C)
+toh(n,'A','C','B', 1)
+print('\nGoal State reached')
